5b37e73fd2d1d411c94a59caaa493fa564f0a5e6
zhiluke001-ux <231238789+zhiluke001-ux@users.noreply.github.com>
2025-09-21 17:18:32 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-
 // pages/_app.tsx
-<Head>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>{SITE_NAME}</title>
-  <link rel="icon" type="image/png" href="/favicon.png" />
-</Head>
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+import { SITE_NAME } from "../utils/constants";
 
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{SITE_NAME}</title>
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+        {/* Favicon (single PNG) */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        {/* Optional extras:
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
