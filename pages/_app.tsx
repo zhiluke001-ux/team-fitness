1b36863d9ada81f1d98d52c689a63daa78e1f8cb
zhiluke001-ux <231238789+zhiluke001-ux@users.noreply.github.com>
2025-09-21 17:15:24 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
+// pages/_app.tsx
+<Head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>{SITE_NAME}</title>
+  <link rel="icon" type="image/png" href="/favicon.png" />
+</Head>
+
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
